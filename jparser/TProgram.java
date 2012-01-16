b0674aa0418dd4a95d3217d6315c1af68aaf93b4
Eric Conlon <ejconlon@gmail.com>
2012-01-15 16:45:55 -0800

    rewriting

---
+++ jparser/TProgram.java
@@ -0,0 +1,25 @@
+package jthrift;
+
+public class TProgram {
+    public void setDoc(String a) {}
+
+    public void setNamespace(String a, String b) {}
+
+    public void addCppInclude(String a) {}
+
+    public void addInclude(String a) {}
+
+    public void addConst(String a) {}
+
+    public void addType(String a, String b) {}
+
+    public void addService(String a) {}
+
+    public void addTypedef(String a) {}
+
+    public void addEnum(String a) {}
+
+    public void addStruct(String a) {}
+
+    public void addXception(String a) {}
+}
