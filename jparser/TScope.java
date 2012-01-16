4a6ea1093e54ef892ae38dda14c916538c45911a
Eric Conlon <ejconlon@gmail.com>
2012-01-16 13:59:34 -0800

    constval

--- jparser/TScope.java
+++ jparser/TScope.java
@@ -3,7 +3,6 @@ package jthrift;
 public class TScope {
     public void addType(Id name, TType type) {}
     public void addService(Id name, TService service) {}
-    public void addConstant(Id name, TConstValue value) {}
     public void addConstant(Id name, TConst value) {}
     public void resolveConstValue(TConstValue value, TType type) {}
 
