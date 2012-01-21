9405cec89f068a662b4e9a6ed44db1ee890ab9db
Eric Conlon <ejconlon@gmail.com>
2012-01-21 11:39:47 -0800

    shuffling

--- jparser/TDoc.java
+++
@@ -1,9 +0,0 @@
-package jthrift.jparser;
-
-public class TDoc {
-    private DText doc;
-
-    public DText getDoc() { return doc; }
-    public void setDoc(DText doc) { this.doc = doc; }
-    public boolean hasDoc() { return doc == null; }
-}
